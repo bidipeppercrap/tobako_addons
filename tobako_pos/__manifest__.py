370abed6f99bde21b017b00c972a65ce911645c2
bidipeppercrap <bidipeppercrap@outlook.com>
2022-03-14 20:35:16 +0700

    Completed POS Receipt interface

--- tobako_pos/__manifest__.py
+++ tobako_pos/__manifest__.py
@@ -9,11 +9,13 @@ Simplify the POS by removing fields that is unnecessary for a retail industry.
     'author': 'bidipeppercrap',
     'depends': ['point_of_sale'],
     'data': [
-        'views/pos_setting.xml'
+        'views/pos_setting.xml',
+        # 'views/pos_config.xml'
     ],
     'assets': {
         'point_of_sale.assets': [
-            '/tobako_pos/static/src/css/style.css',
+            # '/tobako_pos/static/src/css/style.css',
+            '/tobako_pos/static/src/css/override-style.css'
         ],
         # 'web.assets_qweb': [
         #     'tobako_pos/static/src/xml/pos_receipt.xml',
