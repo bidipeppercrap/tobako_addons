af5d6f9dc80c41374a77a9a720c1dc8574402081
bidipeppercrap <bidipeppercrap@outlook.com>
2022-03-14 23:05:57 +0700

    Init RFQ

---
+++ tobako_purchase/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Purchase Simplified',
+    'version': '1.0',
+    'summary': 'Purchase Simplified for Retails',
+    'application': True,
+    'description': """
+Simplify the Purchase menu by removing fields that is unnecessary for a retail industry.
+    """,
+    'author': 'bidipeppercrap',
+    'depends': ['purchase', 'stock'],
+    'data': [
+        'views/rfq.xml'
+    ]
+}
