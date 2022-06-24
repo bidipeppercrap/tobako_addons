4db97ec407e3c34c7593e8984e642e34e089de79
Marshell Wu <marshellwu@proton.me>
2022-06-24 19:49:41 +0700

    Completed Receipt

--- tobako_pos_receipt/__manifest__.py
+++ tobako_pos_receipt/__manifest__.py
@@ -8,11 +8,12 @@
         'point_of_sale.assets': [
             'tobako_pos_receipt/static/src/js/models.js',
             'tobako_pos_receipt/static/src/js/TobakoReceipt.js',
-            'tobako_pos_receipt/static/src/js/TobakoReceiptScreen.js',
+            'tobako_pos_receipt/static/src/js/TobakoReceiptStateButton.js',
             'tobako_pos_receipt/static/src/css/tobako-receipt.css'
         ],
         'web.assets_qweb': [
-            'tobako_pos_receipt/static/src/xml/TobakoReceipt.xml'
+            'tobako_pos_receipt/static/src/xml/TobakoReceipt.xml',
+            'tobako_pos_receipt/static/src/xml/TobakoReceiptStateButton.xml'
         ]
     }
 }
--- tobako_pos_receipt/static/src/js/TobakoReceiptScreen.js
+++
@@ -1,5 +0,0 @@
-odoo.define('tobako_pos_receipt.TobakoReceiptScreen', function (require) {
-    'use strict'
-
-    // const ReceiptScreen = require('point_of_sale.ReceiptScreen')
-})
