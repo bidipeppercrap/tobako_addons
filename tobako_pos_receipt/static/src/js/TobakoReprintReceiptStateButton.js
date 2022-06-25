910c4551b7d5932d985d49c8ea0467dde3564abe
Marshell Wu <marshellwu@proton.me>
2022-06-25 14:41:54 +0700

    Reprint Receipt also has Change State Button

---
+++ tobako_pos_receipt/static/src/js/TobakoReprintReceiptStateButton.js
@@ -0,0 +1,29 @@
+odoo.define('tobako_pos_receipt.TobakoReprintReceiptStateButton', function (require) {
+    'use strict'
+
+    const ReceiptScreen = require('point_of_sale.ReprintReceiptScreen')
+    const Registries = require('point_of_sale.Registries')
+
+    const TobakoReprintReceiptStateButton = (ReceiptScreen) => class extends ReceiptScreen {
+        constructor() {
+            super(...arguments)
+        }
+
+        changeState() {
+            const receiptState = this.orderReceipt.el.getElementsByClassName('tobako-pos-receipt__text--copy')[0]
+            const currentState = receiptState.innerHTML
+            const states = ['normal', '*** COPY ***', '*** ASLI ***']
+            const currentStateIndex = states.indexOf(currentState)
+            const stateIndex = (currentStateIndex + 1) < states.length ? currentStateIndex + 1 : 0
+            const state = states[stateIndex]
+
+            if (stateIndex == 0) receiptState.classList.add('tobako-pos-receipt__hide')
+            else receiptState.classList.remove('tobako-pos-receipt__hide')
+            receiptState.innerHTML = state
+        }
+    }
+
+    Registries.Component.extend(ReceiptScreen, TobakoReprintReceiptStateButton)
+
+    return TobakoReprintReceiptStateButton
+})
