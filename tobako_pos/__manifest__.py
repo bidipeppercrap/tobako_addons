9eeaa8e28e55321f4c875f2e1a2ff1c37cfeb3c4
Marshell Wu <marshellwu@proton.me>
2022-06-25 23:01:06 +0700

    Polishing tobako pos config

--- tobako_pos/__manifest__.py
+++ tobako_pos/__manifest__.py
@@ -1,15 +1,12 @@
 {
-    'name': 'POS Simplified',
+    'name': 'Tobako POS Config',
     'version': '1.1',
-    'summary': 'POS Simplified for Retail',
+    'summary': 'POS config simplified for retail',
     'application': False,
-    'description': """
-Simplify the POS by removing fields that is unnecessary for a retail industry.
-    """,
     'author': 'bidipeppercrap',
     'depends': ['point_of_sale'],
     'data': [
-        'views/pos_setting.xml',
-        # 'views/pos_config.xml'
+        'views/config_hider.xml',
+        'views/config_action.xml'
     ]
 }
