024faabc61c4151904f4f260593b14730d87d759
Marshell Wu <marshellwu@proton.me>
2022-07-04 16:26:11 +0700

    Tobako Stock Report

---
+++ tobako_stock_report/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Tobako Stock Report',
+    'depends': [
+        'base',
+        'stock_account'
+    ],
+    'data': [
+        'report/delivery_slip.xml',
+        'report/delivery_aggregated_move_lines.xml'
+    ]
+}
